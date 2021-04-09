295216c8609304b9027c9edb3af921d97fe5d8a4
AlexanderBanar <s595659@mail.ru>
2021-04-09 06:59:07 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#435793]

--- src/main/java/html/SqlRuParse.java
+++ src/main/java/html/SqlRuParse.java
@@ -7,16 +7,19 @@ import org.jsoup.select.Elements;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        Elements col = doc.select(".altCol");
-        int dateIndex = 1;
-        for (Element td : row) {
-            Element href = td.child(0);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-            System.out.println(col.get(dateIndex).textNodes().get(0));
-            dateIndex += 2;
+        for (int i = 1; i < 6; i++) {
+            String url = String.format("https://www.sql.ru/forum/job-offers/%d", i);
+            Document doc = Jsoup.connect(url).get();
+            Elements row = doc.select(".postslisttopic");
+            Elements col = doc.select(".altCol");
+            int dateIndex = 1;
+            for (Element td : row) {
+                Element href = td.child(0);
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
+                System.out.println(col.get(dateIndex).textNodes().get(0));
+                dateIndex += 2;
+            }
         }
     }
 }
