3b89cd97d5550c10035ab597bb0e39340132008a
AlexanderBanar <s595659@mail.ru>
2021-04-09 07:36:02 +0300

    2.2. Модель данных - Post. [#435794]

---
+++ src/main/java/html/Post.java
@@ -0,0 +1,13 @@
+package html;
+
+import java.util.List;
+
+public class Post {
+    String position;
+    String jobLocation;
+    boolean isRemote;
+    int salaryFrom;
+    int salaryTo;
+    List<String> descriptions;
+    String contact;
+}
