fcccf7ea7fd91122c1ffc8833db6eb110b6cc8d7
AlexanderBanar <s595659@mail.ru>
2021-04-10 08:19:17 +0300

    4. Схема таблицы Post [#435800] _ refactored

--- src/main/java/html/Post.java
+++ src/main/java/html/Post.java
@@ -1,13 +1,59 @@
 package html;
 
-import java.util.List;
+import java.time.LocalDateTime;
 
 public class Post {
-    String position;
-    String jobLocation;
-    boolean isRemote;
-    int salaryFrom;
-    int salaryTo;
-    List<String> descriptions;
-    String contact;
+    private int id;
+    private String name;
+    private String text;
+    private String link;
+    private LocalDateTime created;
+
+    public Post(int id, String name, String text, String link, LocalDateTime created) {
+        this.id = id;
+        this.name = name;
+        this.text = text;
+        this.link = link;
+        this.created = created;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
 }
