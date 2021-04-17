82a109847ea08dcf1901666dbe3323bbc7a06d84
AlexanderBanar <s595659@mail.ru>
2021-04-17 16:46:18 +0300

    6. Grabber. [#435798]

---
+++ src/main/java/html/Grabber.java
@@ -0,0 +1,77 @@
+package html;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.*;
+import java.util.List;
+import java.util.Properties;
+
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
+
+public class Grabber implements Grab {
+    private final Properties cfg = new Properties();
+
+    public Store store() {
+        return new PsqlStore(this.cfg);
+    }
+
+    public Scheduler scheduler() throws SchedulerException {
+        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+        scheduler.start();
+        return scheduler;
+    }
+
+    public void cfg() throws IOException {
+        ClassLoader loader = Grabber.class.getClassLoader();
+        try (InputStream in = loader.getResourceAsStream("app.properties")) {
+            cfg.load(in);
+        }
+    }
+
+    @Override
+    public void init(Parse parse, Store store, Scheduler scheduler) throws SchedulerException {
+        JobDataMap data = new JobDataMap();
+        data.put("store", store);
+        data.put("parse", parse);
+        JobDetail job = newJob(GrabJob.class)
+                .usingJobData(data)
+                .build();
+        SimpleScheduleBuilder times = simpleSchedule()
+                .withIntervalInSeconds(Integer.parseInt(cfg.getProperty("time")))
+                .repeatForever();
+        Trigger trigger = newTrigger()
+                .startNow()
+                .withSchedule(times)
+                .build();
+        scheduler.scheduleJob(job, trigger);
+    }
+
+    public static class GrabJob implements Job {
+
+        @Override
+        public void execute(JobExecutionContext context) throws JobExecutionException {
+            JobDataMap map = context.getJobDetail().getJobDataMap();
+            Store psqlStore = (Store) map.get("store");
+            Parse parseHelper = (Parse) map.get("parse");
+            List<Post> postList = parseHelper.list("https://www.sql.ru/forum/job-offers/5");
+            for (Post currentPost : postList) {
+                psqlStore.save(currentPost);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Grabber grab = new Grabber();
+        grab.cfg();
+        Scheduler scheduler = grab.scheduler();
+        Store store = grab.store();
+        grab.init(new ParseHelper(), store, scheduler);
+        List<Post> resultPostList = store.getAll();
+        for (Post currentPost : resultPostList) {
+            System.out.println(currentPost.getId());
+        }
+    }
+}
